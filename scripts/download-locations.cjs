262a744d403d38a43c50f7dcfeb5a89ce3a104ee
dliadsky <68655932+dliadsky@users.noreply.github.com>
2025-10-10 07:37:21 -0400

    Update download scripts

--- scripts/download-locations.cjs
+++ scripts/download-locations.cjs
@@ -1,64 +1,62 @@
-const
-    https = require("https"),
-    packageId = "1a5be46a-4039-48cd-a2d2-8e702abf9516";
+const https = require("https");
+const fs = require("fs");
+const path = require("path");
 
-// promise to retrieve the package
-const getPackage = new Promise((resolve, reject) => {
-    https.get(`https://ckan0.cf.opendata.inter.prod-toronto.ca/api/3/action/package_show?id=${packageId}`, (response) => {
-        let dataChunks = [];
-        response
-            .on("data", (chunk) => {
-                dataChunks.push(chunk)
-            })
-            .on("end", () => {
-                let data = Buffer.concat(dataChunks)
-                resolve(JSON.parse(data.toString())["result"])
-            })
-            .on("error", (error) => {
-                reject(error)
-            })
+const datasetId = "1a5be46a-4039-48cd-a2d2-8e702abf9516";
+const targetName = "Locations.json";
+const outputPath = path.join("public", "Locations.json");
+
+// Helper to fetch JSON from a URL
+function fetchJSON(url) {
+    return new Promise((resolve, reject) => {
+        https.get(url, (res) => {
+            let data = [];
+            res.on("data", chunk => data.push(chunk));
+            res.on("end", () => {
+                try {
+                    const parsed = JSON.parse(Buffer.concat(data).toString());
+                    resolve(parsed);
+                } catch (err) {
+                    reject(err);
+                }
+            });
+        }).on("error", reject);
     });
-});
+}
 
-getPackage.then(pkg => {
-    // this is the metadata of the package
-    //console.log(pkg);
-}).catch(error => {
-    console.error(error);
-})
-// since this package has resources in the datastore, one can get the data rather than just the metadata of the resources
-// promise to retrieve data of a datastore resource 
-const getDatastoreResource = resource => new Promise((resolve, reject) => {
-    https.get(`https://ckan0.cf.opendata.inter.prod-toronto.ca/api/3/action/datastore_search?id=${resource["id"]}`, (response) => {
-        let dataChunks = [];
-        response
-            .on("data", (chunk) => {
-                dataChunks.push(chunk)
-            })
-            .on("end", () => {
-                let data = Buffer.concat(dataChunks)
-                resolve(JSON.parse(data.toString())["result"]["records"])
-            })
-            .on("error", (error) => {
-                reject(error)
-            })
+// Helper to download a file
+function downloadFile(url, dest) {
+    return new Promise((resolve, reject) => {
+        https.get(url, (res) => {
+            if (res.statusCode !== 200) {
+                reject(new Error(`Failed to download file: ${res.statusCode}`));
+                return;
+            }
+            const file = fs.createWriteStream(dest);
+            res.pipe(file);
+            file.on("finish", () => {
+                file.close(resolve);
+            });
+        }).on("error", reject);
+    });
+}
+
+// Main logic
+fetchJSON(`https://ckan0.cf.opendata.inter.prod-toronto.ca/api/3/action/package_show?id=${datasetId}`)
+    .then(data => {
+        const resources = data.result.resources;
+        const dropInResource = resources.find(r => r.name === targetName);
+
+        if (!dropInResource || !dropInResource.url) {
+            throw new Error(`Resource "${targetName}" not found in dataset.`);
+        }
+
+        console.log(`Downloading ${dropInResource.url}...`);
+        return downloadFile(dropInResource.url, outputPath);
     })
-});
-
-// get the package information again
-getPackage.then(package => {
-    // get the datastore resources for the package
-    let datastoreResources = package["resources"].filter(r => r.datastore_active);
-
-    // retrieve the first datastore resource as an example
-    getDatastoreResource(datastoreResources[0])
-        .then(resource => {
-            // this is the actual data of the resource
-            //console.log(resource)
-        })
-        .catch(error => {
-            console.error(error);
-        })
-}).catch(error => {
-    console.error(error);
-})
+    .then(() => {
+        console.log(`Saved Drop-in data to ${outputPath}`);
+    })
+    .catch(err => {
+        console.error("Error:", err.message);
+    });
