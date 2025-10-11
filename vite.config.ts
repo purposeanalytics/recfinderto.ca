197c068e566ca998d3570401709e056bd46986b0
dliadsky <68655932+dliadsky@users.noreply.github.com>
2025-10-11 18:32:17 -0400

    Update baseurl

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/drop-in-rec/',
+  base: '/',
   plugins: [
     react(),
     tailwindcss(),
