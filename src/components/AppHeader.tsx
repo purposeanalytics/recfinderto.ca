199f3eb5c49e8caf6f576a36c1ad70915c36da2c
dliadsky <68655932+dliadsky@users.noreply.github.com>
2025-10-12 07:41:05 -0400

    Update info modal

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -92,7 +92,7 @@ const AppHeader: React.FC<AppHeaderProps> = ({ onAboutClick }) => {
             <h3 className="text-lg font-semibold text-gray-900 dark:text-slate-200 mb-3">Data source</h3>
             <p className="text-gray-700 dark:text-slate-300 leading-relaxed">
               This tool uses <a className="text-[#13a4ec]" href="https://open.toronto.ca/catalogue/?search=recreation&sort=score%20desc" target="_blank">open data</a> from the City of Toronto's drop-in recreation programs and facilities. 
-              It does not include data about the City's registered recreation programming such as lessons and classes.
+              It does not include information about the City's registered recreation programming such as lessons and classes.
               Data are retrieved nightly to ensure accuracy, but last-minute program changes and availability may not be accuractely reflected here. 
               For the most current information, please contact the recreation centre directly.
             </p>
