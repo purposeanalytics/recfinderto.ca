862c83deb49b96bad7607f176a3e7bf1bbfb04ec
dliadsky <68655932+dliadsky@users.noreply.github.com>
2026-01-22 09:53:29 -0500

    Fix sitemap.xml

--- scripts/generate-sitemap.ts
+++ scripts/generate-sitemap.ts
@@ -66,10 +66,20 @@ try {
 }
 
 // Generate sitemap XML
+// Helper function to escape XML entities in URLs
+const escapeXml = (str: string): string => {
+  return str
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&apos;');
+};
+
 const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${urls.map(url => `  <url>
-    <loc>${url.loc}</loc>
+    <loc>${escapeXml(url.loc)}</loc>
     <lastmod>${url.lastmod}</lastmod>
     <changefreq>${url.changefreq}</changefreq>
     <priority>${url.priority}</priority>
